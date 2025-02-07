38f33dfebe5de280406d009c047772151a41e6b6
jenilson25francisco <franciscoviwaldo@gmail.com>
2025-02-07 03:48:05 +0100

    chore: initial commit

---
+++ src/main/java/com/kandogo/kandogo_api/KandogoApiApplication.java
@@ -0,0 +1,13 @@
+package com.kandogo.kandogo_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KandogoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KandogoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kandogo/kandogo_api/KandogoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kandogo.kandogo_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KandogoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
